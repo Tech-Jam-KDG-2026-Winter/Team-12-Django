e86884fb830eac2be348bb02c47a286eed5254ba
aiueo0123456789 <shunsuke.shiraki@gmail.com>
2026-02-04 21:24:25 +0900

    UI accountsAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """
+    カスタムユーザー管理画面
+    """
+    # 一覧画面で表示するフィールド
+    list_display = ('username', 'email', 'username', 'is_staff', 'last_exercise_time')
+
+    # 詳細画面のフィールドセット
+    fieldsets = UserAdmin.fieldsets + (
+        ('追加情報', {
+            'fields': ('last_exercise_time', 'profile_image')
+        }),
+    )
+
+    # 新規作成画面のフィールドセット
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('追加情報', {
+            'fields': ('last_exercise_time', 'profile_image')
+        }),
+    )
