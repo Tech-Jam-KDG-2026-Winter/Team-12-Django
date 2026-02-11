c00a52e80e064124d5c3e5ed3804cd7df8b7b8ab
aiueo0123456789 <shunsuke.shiraki@gmail.com>
2026-02-11 22:00:49 +0900

    デプロイ試行錯誤

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 AUTH_USER_MODEL = 'accounts.User'
 
